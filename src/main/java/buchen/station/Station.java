fbd2d9aa0a7a948aa8b7ae8df2b6ea893af50bc0
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-12-25 14:30:31 -0500

    updating json variable checkstyles

--- src/main/java/buchen/station/Station.java
+++ src/main/java/buchen/station/Station.java
@@ -3,30 +3,32 @@ package buchen.station;
 public class Station {
 
     //station information
-
-    //CHECKSTYLE:OFF
-
     double lat;
     public double lon;
+    //CHECKSTYLE:OFF
     public String station_id;
+    //CHECKSTYLE:ON
+
     public String name;
 
     //station status
+    //CHECKSTYLE:OFF
     public int num_docks_available;
     public int num_ebikes_available;
     public int num_bikes_available;
+    //CHECKSTYLE:ON
 
 
 
-    public Station(double lat, double lon, String station_id, String name,
-                   int num_docks_available, int num_ebikes_available, int num_bikes_available) {
+
+    public Station(double lat, double lon, String stationId, String name,
+                   int numDocksAvailable, int numEbikesAvailable, int numBikesAvailable) {
         this.lat = lat;
         this.lon = lon;
-        this.station_id = station_id;
+        this.station_id = stationId;
         this.name = name;
-        this.num_docks_available = num_docks_available;
-        this.num_ebikes_available = num_ebikes_available;
-        this.num_bikes_available = num_bikes_available;
+        this.num_docks_available = numDocksAvailable;
+        this.num_ebikes_available = numEbikesAvailable;
+        this.num_bikes_available = numBikesAvailable;
     }
-    //CHECKSTYLE:ON
 }
