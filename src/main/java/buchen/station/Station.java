527dc353366ab3a14d74dee59ce5bb426783871d
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-12-25 14:22:31 -0500

    varialbe with underscores checkstyles

--- src/main/java/buchen/station/Station.java
+++ src/main/java/buchen/station/Station.java
@@ -2,20 +2,22 @@ package buchen.station;
 
 public class Station {
 
-    //CHECKSTYLE:OFF
-
     //station information
     double lat;
     public double lon;
+    //CHECKSTYLE:OFF
     public String station_id;
+    //CHECKSTYLE:ON
     public String name;
 
     //station status
+    //CHECKSTYLE:OFF
     public int num_docks_available;
     public int num_ebikes_available;
     public int num_bikes_available;
-
     //CHECKSTYLE:ON
+
+
 
     public Station(double lat, double lon, String station_id, String name,
                    int num_docks_available, int num_ebikes_available, int num_bikes_available) {
