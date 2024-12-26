9005c63e5e3db939ea13d6efc850e8381c847e83
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-12-25 22:21:20 -0500

    part 2

---
+++ src/main/java/aws/CitiBikeRequestHandler.java
@@ -0,0 +1,36 @@
+package aws;
+
+import buchen.station.CitiBikeService;
+import buchen.station.CitiBikeServiceFactory;
+import buchen.station.FindClosestStation;
+import buchen.station.Station;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.google.gson.Gson;
+
+import java.util.Map;
+
+public class CitiBikeRequestHandler implements RequestHandler<APIGatewayProxyRequestEvent, CitiBikeResponse> {
+
+    @Override
+    public CitiBikeResponse handleRequest(APIGatewayProxyRequestEvent event, Context context) {
+        String body = event.getBody();
+        Gson gson = new Gson();
+        CitiBikeRequest request = gson.fromJson(body, CitiBikeRequest.class);
+
+        CitiBikeServiceFactory factory = new CitiBikeServiceFactory();
+        CitiBikeService service = factory.getService();
+        FindClosestStation stationFinder = new FindClosestStation();
+        Map<String, Station> stationsMap = stationFinder.merge(service);
+
+        CitiBikeResponse response = new CitiBikeResponse();
+        response.from = request.from;
+        response.to = request.to;
+
+        response.start = stationFinder.closestStationAvailableBikes(stationsMap, request.from.lat, request.from.lon);
+        response.end = stationFinder.closestStationAvailableSlots(stationsMap, request.to.lat, request.to.lon);
+
+        return response;
+    }
+}
---
+++ src/main/java/aws/CitiBikeResponse.java
@@ -0,0 +1,10 @@
+package aws;
+
+import buchen.station.Station;
+
+public class CitiBikeResponse {
+    public Point from;
+    public Point to;
+    public Station start;
+    public Station end;
+}
